0ae268044e52feb02130d1661a704b334fd949aa
sofia <2122shb00002@iacademy.edu.ph>
2022-01-22 02:48:08 +0800

    test

---
+++ app/Http/Livewire/Counter.php
@@ -0,0 +1,16 @@
+<?php
+
+class Counter extends Component
+{
+    public $count = 0;
+ 
+    public function increment()
+    {
+        $this->count++;
+    }
+ 
+    public function render()
+    {
+        return view('livewire.counter');
+    }
+}
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,32 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    Leche ka!
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
+<!--After reaching the pitstop for Mile 9 your task is to design a simple for letting the user input information (full name, birthday, address) 
+Output 
+Fullname:
+Address: 
+Zodiac Sign: (use control structure Switch Statement for the condition) 
+Sample Output 
+Hi: Raden Alvarez
+Your Address is: makati city 
+Zodiac Sign (Aries) 
+*Things to consider 
+Use Control Structure condition (Switch statement)
+PHP 
+Form 
+*External CSS
+*JavaScript-->
